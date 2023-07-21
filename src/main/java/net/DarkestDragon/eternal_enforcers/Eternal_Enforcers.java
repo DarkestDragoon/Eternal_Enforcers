382400440239d3c0b1d402ce83f950d444557b62
sean tallman <gabrieltallman2006@gmail.com>
2023-07-21 14:41:12 -0700

    Initial commit

---
+++ src/main/java/net/DarkestDragon/eternal_enforcers/Eternal_Enforcers.java
@@ -0,0 +1,35 @@
+package net.DarkestDragon.eternal_enforcers;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Eternal_Enforcers.MOD_ID)
+public class Eternal_Enforcers {
+    public static final String MOD_ID = "eternal_enforcers";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public Eternal_Enforcers() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
